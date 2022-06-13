158e6d9a0d9355160695535dddf0fac528f8cbb2
Szymon Sus <sus.szymon3@gmail.com>
2022-06-13 20:28:35 +0200

    fix: missing icons and site config

---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  siteName: 'Offer Exchange',
+  siteUrl: 'https://offerexchange.net',
+  twitterUrl: 'https://twitter.com/offer_exchange',
+  faceBookUrl: 'https://facebook.com/offerexchange',
+  copyright: `© ${new Date().getFullYear()} CONETSO Sp. z o. o.`,
+};
