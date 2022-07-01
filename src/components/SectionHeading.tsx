8ca9aa18d204e7b0a1d108190a2c8a8da86d02a2
Szymon Sus <sus.szymon3@gmail.com>
2022-07-01 18:49:14 +0200

    refactor: change section heading and features styles

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -12,14 +12,12 @@ export default function SectionHeading({
   className,
 }: SectionHeadingProps) {
   return (
-    <div className={clsxm('my-14 text-center md:my-20', className)}>
+    <div className={clsxm('my-14 mx-auto max-w-4xl text-center md:my-20', className)}>
       {!!heading && (
-        <h3 className='mb-4 text-2xl font-bold text-primary md:mb-5 md:text-3xl'>
-          {heading}
-        </h3>
+        <h3 className='text-2xl font-extrabold text-primary sm:text-4xl'>{heading}</h3>
       )}
       {!!subheading && (
-        <p className='text-base font-light text-slate-700 md:text-lg'>{subheading}</p>
+        <p className='mt-3 text-base text-gray-500 sm:mt-4 md:text-xl'>{subheading}</p>
       )}
     </div>
   );
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -52,7 +52,7 @@ const FEATURES_DATA: Feature[] = [
 
 export default function Features() {
   return (
-    <section id='features' className='py-8 lg:py-20'>
+    <section id='features' className='bg-gray-50 py-8 lg:py-20'>
       <Container>
         <SectionHeading
           heading='Jakie funkcje oferuje nasz produkt?'
