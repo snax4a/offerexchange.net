6742ba297bfa65fba099923b2281778f0156f3d6
Szymon Sus <sus.szymon3@gmail.com>
2022-07-01 13:32:41 +0200

    refactor: display social banner conditionally

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -7,18 +7,16 @@ interface PageProps {
   children: React.ReactNode;
 }
 
-const bannerText = 'Beta testy ruszyły!';
-const bannerLink = '/sign-up-for-beta';
-const bannerLinkText = 'Zapisz się na listę oczekujących.';
-
 export default function Page({ children }: PageProps) {
   return (
     <>
-      <SocialBanner
-        bannerText={bannerText}
-        bannerLink={bannerLink}
-        bannerLinkText={bannerLinkText}
-      />
+      {process.env.NEXT_PUBLIC_SHOW_SOCIAL_BANNER === 'true' && (
+        <SocialBanner
+          bannerText={process.env.NEXT_PUBLIC_SOCIAL_BANNER_TEXT ?? ''}
+          bannerLink={process.env.NEXT_PUBLIC_SOCIAL_BANNER_LINK ?? ''}
+          bannerLinkText={process.env.NEXT_PUBLIC_SOCIAL_BANNER_LINK_TEXT ?? ''}
+        />
+      )}
       <div className='flex h-auto flex-row lg:h-screen'>
         {/* No fallback UI so need to be careful not to suspend directly inside. */}
         <React.Suspense fallback={null}>
