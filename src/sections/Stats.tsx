e45e449c02c9d9a75f0ee6ce31a9b503553f0987
Szymon Sus <sus.szymon3@gmail.com>
2022-07-03 11:22:04 +0200

    refactor: update stats styles

--- src/sections/Stats.tsx
+++ src/sections/Stats.tsx
@@ -5,19 +5,21 @@ import SectionHeading from '@/components/SectionHeading';
 
 export default function Stats() {
   return (
-    <section id='stats' className='bg-gray-50 pt-12 sm:pt-16'>
+    <section id='stats' className='bg-gray-50 pt-12 md:pt-16'>
       <Container>
         <SectionHeading
           heading='Zaufało nam już ponad 40 firm'
           subheading='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus repellat laudantium.'
         />
-        <div className='mt-10 bg-white pb-12 sm:pb-16'>
-          <div className='relative'>
-            <div className='absolute inset-0 h-1/2 bg-gray-50' />
-            <div className='relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+      </Container>
+      <div className='mt-10 bg-white pb-12 md:pb-16'>
+        <div className='relative'>
+          <div className='absolute inset-0 h-1/2 bg-gray-50' />
+          <div className='relative mx-auto max-w-7xl px-4 md:px-6 lg:px-8'>
+            <Container>
               <div className='mx-auto max-w-4xl'>
-                <dl className='rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3'>
-                  <div className='flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r'>
+                <dl className='rounded-2xl bg-white shadow-lg md:grid md:grid-cols-3'>
+                  <div className='flex flex-col border-b border-gray-100 p-6 text-center md:border-0 md:border-r'>
                     <dt className='order-2 mt-2 text-lg font-medium leading-6 text-gray-500'>
                       Wysłanych zapytań
                     </dt>
@@ -25,7 +27,7 @@ export default function Stats() {
                       ~200
                     </dd>
                   </div>
-                  <div className='flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r'>
+                  <div className='flex flex-col border-t border-b border-gray-100 p-6 text-center md:border-0 md:border-l md:border-r'>
                     <dt className='order-2 mt-2 text-lg font-medium leading-6 text-gray-500'>
                       Zebranych ofert
                     </dt>
@@ -33,7 +35,7 @@ export default function Stats() {
                       ~800
                     </dd>
                   </div>
-                  <div className='flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l'>
+                  <div className='flex flex-col border-t border-gray-100 p-6 text-center md:border-0 md:border-l'>
                     <dt className='order-2 mt-2 text-lg font-medium leading-6 text-gray-500'>
                       Zaoszczędzonego czasu
                     </dt>
@@ -43,10 +45,10 @@ export default function Stats() {
                   </div>
                 </dl>
               </div>
-            </div>
+            </Container>
           </div>
         </div>
-      </Container>
+      </div>
     </section>
   );
 }
