fafacb4f7934d094525f030459f58dffaf08fc4d
Szymon Sus <sus.szymon3@gmail.com>
2022-07-05 17:15:57 +0200

    refactor: extract bg pattern svgs into one svg file

--- src/pages/rules.tsx
+++ src/pages/rules.tsx
@@ -8,7 +8,9 @@ import Seo from '@/components/Seo';
 
 import { siteConfig } from '@/config/site';
 
-export default function Rules() {
+import SquaresPattern from '~/svg/squares-pattern.svg';
+
+export default function RulesPage() {
   return (
     <Page>
       <Seo title={`${siteConfig.siteName} | Regulamin aplikacji`} />
@@ -16,102 +18,21 @@ export default function Rules() {
       <div className='relative overflow-hidden bg-white py-16'>
         <div className='hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full'>
           <div className='relative mx-auto h-full max-w-prose text-lg' aria-hidden='true'>
-            <svg
-              className='absolute top-12 left-full translate-x-32 transform'
+            <SquaresPattern
               width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='74b3fd99-0a6f-4271-bef2-e80eeafdf357'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-200'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill='url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)'
-              />
-            </svg>
-            <svg
-              className='absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform'
+              height={450}
+              className='absolute top-12 left-full translate-x-32 transform text-secondary/20'
+            />
+            <SquaresPattern
               width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='f210dbf6-a58d-4871-961e-36d5016a0f49'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-200'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill='url(#f210dbf6-a58d-4871-961e-36d5016a0f49)'
-              />
-            </svg>
-            <svg
-              className='absolute bottom-12 left-full translate-x-32 transform'
+              height={450}
+              className='absolute top-1/2 right-full -translate-y-1/2 -translate-x-32 transform text-secondary/20'
+            />
+            <SquaresPattern
               width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='d3eb07ae-5182-43e6-857d-35c643af9034'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-200'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill='url(#d3eb07ae-5182-43e6-857d-35c643af9034)'
-              />
-            </svg>
+              height={450}
+              className='absolute bottom-12 left-full translate-x-32 transform text-secondary/20'
+            />
           </div>
         </div>
         <div className='relative px-4 text-justify sm:px-6 lg:px-8'>
