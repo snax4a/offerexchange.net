e833c51b654fb2421a26eb6d380d06602c7bb93c
Szymon Sus <sus.szymon3@gmail.com>
2022-07-05 13:02:36 +0200

    feat: close popover on mobile nav link click

--- src/components/navigation/MobileNavigation.tsx
+++ src/components/navigation/MobileNavigation.tsx
@@ -24,74 +24,82 @@ export default function MobileNavigation() {
         focus
         className='absolute inset-x-0 top-0 z-50 origin-top-right transform py-2 pt-4 transition md:hidden'
       >
-        <div className='divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5'>
-          <div className='px-5 pt-5 pb-6'>
-            <div className='flex items-center justify-between'>
-              <UnstyledLink href={ROUTES.HOME} className='focus-visible:ring-0'>
-                <Logo className='w-44 text-9xl xl:w-48' height='auto' width='auto' />
-              </UnstyledLink>
-              <div className='-mr-2'>
-                <Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:!outline-none focus-visible:ring focus-visible:ring-opacity-75'>
-                  <span className='sr-only'>Zamknij menu</span>
-                  <XIcon className='h-6 w-6' aria-hidden='true' />
-                </Popover.Button>
+        {({ close }) => (
+          <div className='divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5'>
+            <div className='px-5 pt-5 pb-6'>
+              <div className='flex items-center justify-between'>
+                <UnstyledLink
+                  href={ROUTES.HOME}
+                  onClick={() => close()}
+                  className='focus-visible:ring-0'
+                >
+                  <Logo className='w-44 text-9xl xl:w-48' height='auto' width='auto' />
+                </UnstyledLink>
+                <div className='-mr-2'>
+                  <Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:!outline-none focus-visible:ring focus-visible:ring-opacity-75'>
+                    <span className='sr-only'>Zamknij menu</span>
+                    <XIcon className='h-6 w-6' aria-hidden='true' />
+                  </Popover.Button>
+                </div>
+              </div>
+
+              <div className='mt-6'>
+                <nav className='grid gap-6'>
+                  {featureItems.map((item) => (
+                    <UnstyledLink
+                      key={item.name}
+                      href={item.href}
+                      onClick={() => close()}
+                      className='-m-3 flex items-center rounded-lg p-2 hover:bg-gray-50 focus-visible:ring-inset sm:p-3'
+                    >
+                      <div className='flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white'>
+                        <item.icon className='h-6 w-6' aria-hidden='true' />
+                      </div>
+                      <div className='ml-4 text-sm font-medium text-gray-900 sm:text-base'>
+                        {item.name}
+                      </div>
+                    </UnstyledLink>
+                  ))}
+                </nav>
               </div>
             </div>
 
-            <div className='mt-6'>
-              <nav className='grid gap-6'>
-                {featureItems.map((item) => (
+            <div className='py-6 px-5'>
+              <div className='grid grid-cols-2 gap-4'>
+                {navigationItems.map((item) => (
                   <UnstyledLink
                     key={item.name}
                     href={item.href}
-                    className='-m-3 flex items-center rounded-lg p-2 hover:bg-gray-50 focus-visible:ring-inset sm:p-3'
+                    onClick={() => close()}
+                    className='rounded-sm text-sm font-medium text-gray-900 hover:text-gray-700 focus-visible:!ring-offset-4 sm:text-base'
                   >
-                    <div className='flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white'>
-                      <item.icon className='h-6 w-6' aria-hidden='true' />
-                    </div>
-                    <div className='ml-4 text-sm font-medium text-gray-900 sm:text-base'>
-                      {item.name}
-                    </div>
+                    {item.name}
                   </UnstyledLink>
                 ))}
-              </nav>
-            </div>
-          </div>
+              </div>
 
-          <div className='py-6 px-5'>
-            <div className='grid grid-cols-2 gap-4'>
-              {navigationItems.map((item) => (
-                <UnstyledLink
-                  key={item.name}
-                  href={item.href}
-                  className='rounded-sm text-sm font-medium text-gray-900 hover:text-gray-700 focus-visible:!ring-offset-4 sm:text-base'
-                >
-                  {item.name}
-                </UnstyledLink>
-              ))}
-            </div>
-
-            <div className='mt-6'>
-              <UnstyledLink
-                openNewTab={false}
-                href={ROUTES.REGISTER}
-                className='flex w-full items-center justify-center rounded-md border border-transparent bg-secondary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700'
-              >
-                Utwórz konto
-              </UnstyledLink>
-              <p className='mt-6 text-center text-base font-medium text-gray-500'>
-                Masz już konto?{' '}
+              <div className='mt-6'>
                 <UnstyledLink
                   openNewTab={false}
-                  href={ROUTES.LOGIN}
-                  className='rounded-sm text-secondary hover:text-indigo-600 focus-visible:ring-offset-2'
+                  href={ROUTES.REGISTER}
+                  className='flex w-full items-center justify-center rounded-md border border-transparent bg-secondary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700'
                 >
-                  Zaloguj się
+                  Utwórz konto
                 </UnstyledLink>
-              </p>
+                <p className='mt-6 text-center text-base font-medium text-gray-500'>
+                  Masz już konto?{' '}
+                  <UnstyledLink
+                    openNewTab={false}
+                    href={ROUTES.LOGIN}
+                    className='rounded-sm text-secondary hover:text-indigo-600 focus-visible:ring-offset-2'
+                  >
+                    Zaloguj się
+                  </UnstyledLink>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </Popover.Panel>
     </Transition>
   );
