b75e27a1c5722d4e8fa4bdc585f1e6523ea008dc
Szymon Sus <sus.szymon3@gmail.com>
2022-07-05 17:58:56 +0200

    feat: replace favicons, manifest and browserconfig

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -63,95 +63,18 @@ export default function Seo(props: SeoProps) {
       )}
 
       {/* Favicons */}
-      {favicons.map((linkProps) => (
-        <link key={linkProps.href} {...linkProps} />
-      ))}
-      <meta name='msapplication-TileColor' content='#ffffff' />
-      <meta name='msapplication-TileImage' content='/favicon/ms-icon-144x144.png' />
+      {/* Webhint */}
+      <link rel='apple-touch-icon' sizes='180x180' href='/meta/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/meta/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/meta/favicon-16x16.png' />
+      <link rel='manifest' href='/meta/site.webmanifest' />
+      <link rel='mask-icon' href='/meta/safari-pinned-tab.svg' color='#5bbad5' />
+      <link rel='shortcut icon' href='/meta/favicon.ico' />
+      <meta name='apple-mobile-web-app-title' content='Offer Exchange' />
+      <meta name='application-name' content='Offer Exchange' />
+      <meta name='msapplication-TileColor' content='#ffc40d' />
+      <meta name='msapplication-config' content='/meta/browserconfig.xml' />
       <meta name='theme-color' content='#ffffff' />
     </Head>
   );
 }
-
-type Favicons = {
-  rel: string;
-  href: string;
-  sizes?: string;
-  type?: string;
-};
-
-const favicons: Array<Favicons> = [
-  {
-    rel: 'apple-touch-icon',
-    sizes: '57x57',
-    href: '/favicon/apple-icon-57x57.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '60x60',
-    href: '/favicon/apple-icon-60x60.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '72x72',
-    href: '/favicon/apple-icon-72x72.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '76x76',
-    href: '/favicon/apple-icon-76x76.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '114x114',
-    href: '/favicon/apple-icon-114x114.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '120x120',
-    href: '/favicon/apple-icon-120x120.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '144x144',
-    href: '/favicon/apple-icon-144x144.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '152x152',
-    href: '/favicon/apple-icon-152x152.png',
-  },
-  {
-    rel: 'apple-touch-icon',
-    sizes: '180x180',
-    href: '/favicon/apple-icon-180x180.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '192x192',
-    href: '/favicon/android-icon-192x192.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '32x32',
-    href: '/favicon/favicon-32x32.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '96x96',
-    href: '/favicon/favicon-96x96.png',
-  },
-  {
-    rel: 'icon',
-    type: 'image/png',
-    sizes: '16x16',
-    href: '/favicon/favicon-16x16.png',
-  },
-  {
-    rel: 'manifest',
-    href: '/favicon/manifest.json',
-  },
-];
