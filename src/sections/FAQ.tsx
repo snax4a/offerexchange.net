351de5ff26c9691e18f31c5bad628427c0e651bb
Szymon Sus <sus.szymon3@gmail.com>
2022-07-10 11:42:23 +0200

    feat(faq): remove unwanted faq item

--- src/sections/FAQ.tsx
+++ src/sections/FAQ.tsx
@@ -73,19 +73,6 @@ const accordionData: AccordionItem[] = [
       </div>
     ),
   },
-  {
-    isExpanded: false,
-    title: 'How do I get the payment complete?',
-    contents: (
-      <div>
-        For our recent trip to S.A. I booked several accommodation thru SA Places. I just
-        wanted to tell you that everything worked out perfectly with all the bookings and
-        also your booking was very quick and professional. I hope I have the opportunity
-        to re-visit South Africa soon, I will then make my bookings with your company
-        again. I will also recommend
-      </div>
-    ),
-  },
 ];
 
 export default function FAQ() {
