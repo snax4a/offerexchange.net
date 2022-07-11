a88c5d23df4e3343bd4e729454899401301a8c74
Szymon Sus <sus.szymon3@gmail.com>
2022-07-11 20:04:26 +0200

    feat(analytics): add plausible provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import { AppProps } from 'next/app';
+import PlausibleProvider from 'next-plausible';
 import 'focus-visible';
 
 import 'keen-slider/keen-slider.min.css';
 import '@/styles/globals.css';
 
 function OfferExchange({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PlausibleProvider
+      selfHosted
+      trackOutboundLinks
+      domain='offerexchange.net'
+      customDomain='analytics.offerexchange.net'
+      enabled={process.env.NODE_ENV === 'production'}
+    >
+      <Component {...pageProps} />;
+    </PlausibleProvider>
+  );
 }
 
 export default OfferExchange;
