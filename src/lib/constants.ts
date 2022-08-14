41134c0fa218664e519eecd80d7d7e1d64bf16fd
Szymon Sus <sus.szymon3@gmail.com>
2022-08-14 09:47:44 +0200

    feat: change app domains

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -15,7 +15,7 @@ export const ROUTES = {
   SUPPORT: '/support',
   RULES: '/rules',
   PRIVACY_POLICY: '/privacy-policy',
-  LOGIN: 'https://beta.offerexchange.net/auth/login',
-  REGISTER: 'https://beta.offerexchange.net/auth/register',
+  LOGIN: 'https://app.offerexchange.net/auth/login',
+  REGISTER: 'https://app.offerexchange.net/auth/register',
   NOT_FOUND: '/404',
 };
